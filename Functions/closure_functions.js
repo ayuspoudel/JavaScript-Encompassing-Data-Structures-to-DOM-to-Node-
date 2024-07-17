9abc2d608edb54acd7b66b29145dbd4bbb01b614
Ayush Poudel <ayushpoudel@usf.edu>
2024-07-17 18:44:03 -0400

    Add

--- Functions/closure_functions.js
+++ Functions/closure_functions.js
@@ -1,1 +1,9 @@
-function wrapvalue()
+function wrapvalue(n){
+    var localVariable = n;
+    return function(){
+        return localVariable;
+    }
+}
+var wrap1 = wrapvalue(1);
+var wrap2 = wrapvalue(2);
+console.log(wrap1());
--- closure_functions.js
+++
@@ -1,9 +0,0 @@
-function wrapvalue(n){
-    var localVariable = n;
-    return function(){
-        return localVariable;
-    }
-}
-var wrap1 = wrapvalue(1);
-var wrap2 = wrapvalue(2);
-console.log(wrap1());
