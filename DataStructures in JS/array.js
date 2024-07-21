3b4ca18c3114385f0f162094534955e448d0f6e3
Ayush Poudel <ayushpoudel@usf.edu>
2024-07-21 15:18:39 -0400

    Add array code file to directory

--- DataStructures in JS/array.js
+++ DataStructures in JS/array.js
@@ -18,4 +18,31 @@ let search = (arr, key) => {
     return -1;
 }
 
+let insertionpoint = (arr, key) =>{
+    let start = 0;
+    let end = arr.length -1;
+    let mid = start + (end-start)/2;
+    while(start<=end){
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid>key]){
+            start = mid +1;
+        }
+        {
+            end =  mid -1;
+        }
+    }
+    return start;
+}
+
+let push = (arr, key) =>{
+    let pos = insertionpoint(arr, key);
+    for(let i = arr.length -1; i>=pos;i--){
+        arr[i+1] = arr[i];
+    }
+    console.log(arr);
+}
+
 console.log(search(newArray, 20));
+push(newArray, 15);
