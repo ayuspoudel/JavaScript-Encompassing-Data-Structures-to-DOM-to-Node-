a0cfa71b88c0fb59c48112d0d25374603adbce40
Ayush Poudel <ayushpoudel@usf.edu>
2024-07-24 13:27:36 -0400

    Add Asynchronous JS Module to the code base

---
+++ Asynchronous-Javascript/eventloop.js
@@ -0,0 +1,16 @@
+// {
+//     (function(){
+//         setTimeout(() => {
+//             console.log("Start")
+//         }, 200)
+//         setTimeout(()=>{
+//             console.log("Second Start")
+//         }, 300);
+//     })();
+//     console.log("End");
+// }
+
+{
+    setTimeout(()=>{console.log("Ayush")}, 2000);
+    setTimeout(()=>{console.log("Rakshya")},400 );
+}
